b1969ea6a27e69ba918579452e2fdaad55d8445d
jpomfret <jpomfret7@gmail.com>
2023-05-12 07:08:39 +0000

    fancy error handling

--- functions/NewStorageAcct/run.ps1
+++ functions/NewStorageAcct/run.ps1
@@ -6,22 +6,37 @@ param($Request, $TriggerMetadata)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
-# Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
+try {
 
-#if (-not $name) {
-#    $name = $Request.Body.Name
-#}
+    $name = $Request.Query.Name
+    $sku = 'Standard_LRS'
+    $location = 'uksouth'
 
-$body = [PSCustomObject]@{
-    StorageAccountName = $Name
-    Test = 'Hi'
+    $splatStorage = @{
+        Name = $name
+        ResourceGroupName = 'psconfeu-rg'
+        Location = $location
+        SkuName = $sku
+    }
+    $results = New-AzStorageAccount @splatStorage
+
+    $body = [PSCustomObject]@{
+        StorageAccountName = $Name
+        ProvisioningState =  $results.ProvisioningState
+        CreationTime = CreationTime
+    }
+} catch {
+    $body = [PSCustomObject]@{
+        Error = $_.Exception.Message
+        #Success = $false
+        ProvisioningState =  $results.ProvisioningState
+    }
+} finally {
+
+    # Associate values to output bindings by calling 'Push-OutputBinding'.
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::OK
+        Body = (ConvertTo-Json $body)
+    })
+
 }
-
-New-AzStorageAccount -ResourceGroupName psconfeu-rg -Name $name -Location uksouth -SkuName Standard_LRS
-
-# Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body = (ConvertTo-Json $body)
-})
