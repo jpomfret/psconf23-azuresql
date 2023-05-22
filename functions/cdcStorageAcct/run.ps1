30ee3dcfd5124bd4e41ccde7b3849ce08d51e400
jpomfret <jpomfret7@gmail.com>
2023-05-22 15:32:35 +0000

    cleanup

--- functions/cdcStorageAcct/run.ps1
+++ functions/cdcStorageAcct/run.ps1
@@ -1,28 +1,4 @@
 using namespace System.Net
-#
-## Input bindings are passed in via param block.
-#param($Request, $TriggerMetadata)
-#
-## Write to the Azure Functions log stream.
-#Write-Host "PowerShell HTTP trigger function processed a request."
-#
-## Interact with query parameters or the body of the request.
-#$name = $Request.Query.Name
-#if (-not $name) {
-#    $name = $Request.Body.Name
-#}
-#
-#$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-#
-#if ($name) {
-#    $body = "Hello, $name. This HTTP triggered function executed successfully."
-#}
-#
-## Associate values to output bindings by calling 'Push-OutputBinding'.
-#Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-#    StatusCode = [HttpStatusCode]::OK
-#    Body = $body
-#})
 
 param($stgAcctChanges)
 # The output is used to inspect the trigger binding parameter in test methods.
@@ -49,12 +25,7 @@ try {
         }
         $results = New-AzStorageAccount @splatStorage
     }
-    #$body = [PSCustomObject]@{
-    #    StorageAccountName = $Name
-    #    ProvisioningState  = $results.ProvisioningState
-    #    CreationTime       = $results.CreationTime
-    #    Tags               = $results.Tags
-    #}
+
     $body = [PSCustomObject]@{ 
         logMessage = ('{0} created - {1}' -f $order.storageAcctName, $results.ProvisioningState)
     }
