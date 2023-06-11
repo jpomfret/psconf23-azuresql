6ea0f5a896111d470a7c06ea33f725cb1fab8319
jpomfret <jpomfret7@gmail.com>
2023-06-11 13:24:53 +0000

    remove comments

--- functions/cdcStorageAcct/run.ps1
+++ functions/cdcStorageAcct/run.ps1
@@ -3,19 +3,10 @@ using namespace System.Net
 param($stgAcctChanges)
 
 Import-Module Az.Storage
-# The output is used to inspect the trigger binding parameter in test methods.
-# Use -Compress to remove new lines and spaces for testing purposes.
-#$changesJson = $stgAcctChanges | ConvertTo-Json -Compress
-#Write-Host "SQL Changes: $changesJson"
 
 try {
 
-    #$stgAcctChanges[0]
-    #$stgAcctChanges | Measure-Object
-    
-
     $logMessage = 'Creating storage account(s): '
-    # foreach change create a storage account
     foreach ($change in $stgAcctChanges) {
         Write-Host ("Change operation: {0}" -f $change.Operation)
         Write-Host ("Create a storage account ID: {0}, Name: {1}" -f $change.Item.storageAcctId, $change.Item.storageAcctName)
@@ -41,12 +32,10 @@ try {
 } catch {
     
     write-error $_.Exception
-    #write-error $_.Exception.Message
     $body = [PSCustomObject]@{
         logMessage = ('{0} - {1}' -f $results.ProvisioningState, $_.Exception.Message)
     }
 } finally {
     # Push output to the log table.
     Push-OutputBinding -Name log -Value $body
-    
 }
